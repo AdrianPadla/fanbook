bed746d688d38cc8ad85300d28ca774fa81cbb38
AdrianPadla <adrianpadlasantika@gmail.com>
2023-05-17 23:04:10 +0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from os.path import join, dirname
+from os.path import join,dirname
 from dotenv import load_dotenv
-
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -14,27 +14,32 @@ DB_NAME = os.environ.get("DB_NAME")
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
+@app.route("/mars", methods=["POST"])
+def web_mars_post():
     name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+
     doc = {
         'name': name_receive,
-        'comment': comment_receive,
+        'address': address_receive,
+        'size': size_receive,
     }
-    db.fanmessages.insert_one(doc)
-    return jsonify({'msg':'POST request!'})
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    message_list = list(db.fanmessages.find({}, {'_id': False}))
-    return jsonify({'messages': message_list})
+    db.orders.insert_one(doc)
+    return jsonify({'msg': 'complete!'})
+
+@app.route("/mars", methods=["GET"])
+def web_mars_get():
+    orders_list = list(db.orders.find({},{'_id':False}))
+    return jsonify({'orders':orders_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
